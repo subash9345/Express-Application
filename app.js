9f3f0e670a15d74fea986678d5e203146081aebd
subash1303 <subash@bezohminds.com>
2025-06-03 13:03:23 +0530

    cors fix

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const connectDB = require('./db/db.js');
 const cartRoutes = require('./routes/cartRoutes');
 const port = process.env.PORT || 3007;;
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // allow all origins
+}));
 app.use(express.json());
 setupSwagger(app);
 connectDB();
